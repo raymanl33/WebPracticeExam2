260a983bcb2da0a3285287f34f9f89f3e2040317
Raymond Lee <raymanl33@icloud.com>
2021-11-26 22:09:32 -0800

    add if dir exist check in the unzip function

--- starter/IOhandler.js
+++ starter/IOhandler.js
@@ -27,9 +27,14 @@ const unzipper = require("unzipper"),
  */
 const unzip = (pathIn, pathOut) => {
   return new Promise((resolve, reject) => { 
-    fs.createReadStream(`${pathIn}`)
-    .pipe(unzipper.Extract({ path: `${pathOut}` }))
-    resolve('Extraction Operation Complete') // MAKE SURE TO ONLY CONSOLE.LOG AFTER CREATING THE FOLDER
+    if (fs.existsSync(pathOut)) {
+      resolve(`${pathOut} DIR exists moving to the readDir function...`) 
+    } else {
+      fs.createReadStream(`${pathIn}`)
+      .pipe(unzipper.Extract({ path: `${pathOut}` }))
+      resolve('Extraction Operation Complete')
+    }
+
   })
 };
 
@@ -68,7 +73,7 @@ const readDir = (dir) => {
  * @return {promise}
  */
 const grayScale = (pathIn, pathOut) => {
-  // console.log(pathIn)
+  
   return new Promise((resolve, reject) => {
     pathIn.forEach(pngPath => {
       fs.createReadStream(pngPath)
@@ -88,49 +93,13 @@ const grayScale = (pathIn, pathOut) => {
             this.data[idx + 3] = this.data[idx + 3] >> 1;
           }
         }
-     
+       
         this.pack().pipe(fs.createWriteStream(pathOut))
-        
       });
     })
   })
 };
 
-
-// const grayScale = (pathIn, pathOut) => {
-  
- 
-//   // console.log(pathOut)
-//   return new Promise((resolve, reject) => {
-//     pathIn.forEach(image => {
-//       fs.createReadStream(image)
-//       .pipe(
-//         new PNG({
-//           filterType: 4,
-//         })
-//       )
-//       .on("parsed", function () {
-//         for (var y = 0; y < this.height; y++) {
-//           for (var x = 0; x < this.width; x++) {
-//             var idx = (this.width * y + x) << 2;
-     
-//             // invert color
-//             this.data[idx] = 255 - this.data[idx];
-//             this.data[idx + 1] = 255 - this.data[idx + 1];
-//             this.data[idx + 2] = 255 - this.data[idx + 2];
-     
-//             // and reduce opacity
-//             this.data[idx + 3] = this.data[idx + 3] >> 1;
-//           }
-//         }
-     
-//         this.pack().pipe(fs.createWriteStream(pathOut))
-//         resolve('done')
-//       });
-//     })
-  
-//   })
-// };
 
 module.exports = {
   unzip,
